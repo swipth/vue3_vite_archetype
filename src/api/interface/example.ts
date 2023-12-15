372bffc07269c3b86e5df794fe9844cfc432f9ca
swipth <13218037380@163.com>
2023-12-15 13:31:37 +0800

    feat: init

--- src/api/interface/example.ts
+++ src/api/interface/example.ts
@@ -1,3 +1,10 @@
-import { ajaxGet } from './index'
-// 出库 状态是2 表示出库 1是待领用
-export const receive = (orderId:string) => ajaxGet('/api/mall/outstock', { orderId })
+import {ajaxGet, ajaxPost} from "./index";
+
+// 参考示例编写http请求
+export const ExampleApi = {
+  // 参数名称简洁详细列出参数分界线为4个
+  receive: (orderId: string) => ajaxGet("/api/mall/outstock", {orderId}),
+  // 以下为超出4个参数显示情况  使用filterParams省略显示 需要做类型控制
+  receive2: (filterParams: {name:string;age:number;sex:number;nation:string}) => ajaxPost("/api/mall/outstock", filterParams)
+}
+

7d757fcc4ca4e6816b611f9937fbc6dfef7d1335
swipth <13218037380@163.com>
2023-12-18 14:51:46 +0800

    feat: init

--- src/api/version/update.js
+++ src/api/version/update.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { currentVersion } = require("./info.js");
 // 选择 env 文件 判断打包环境指定对应的服务器id
-const envFile = process.argv[2] === "production" ? "../.env.production" : "../.env.test";
+const envFile = process.argv[2] === "production" ? "../../../.env.production" : "../../../.env.test";
 // env环境变量的路径
 const envPath = path.resolve(__dirname, envFile);
 // env对象 读取文件
@@ -10,7 +10,7 @@ const envObj = parse(fs.readFileSync(envPath, "utf8"));
 const writeVersion = () => {
   // 写入文件
   fs.writeFile(
-    path.join(__dirname, "../" + envObj.VITE_OUT_PUT_NAME + "/static/version.json"),
+    path.join(__dirname, "../../../" + envObj.VITE_OUT_PUT_NAME + "/static/version.json"),
     JSON.stringify({
       success: true,
       data: { version: currentVersion },
